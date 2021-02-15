29469c50caa35c2e188ecece1716bbe81bf2e97a
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-15 10:00:00 +0530

    created constant color file using hexcode

---
+++ colors.constant.js
@@ -0,0 +1,10 @@
+export const colors = [
+    '#4063FA', //blue
+    '#E74C3C', //red
+    '#28B463', //green
+    '#F4F124', //yellow
+    '#F545F3', //pink
+    '#45F5F5', //lignt-blue
+    '#895252', //brown
+    '#B4ADAD' // grey
+];
