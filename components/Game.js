2a4e2de693d72d383481c4c7daefbc341159f494
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-15 11:33:09 +0530

    changing the cell color to picked color

--- components/Game.js
+++ components/Game.js
@@ -1,20 +1,42 @@
 import React, { useState } from "react";
 import Row from "../components/Row";
 
-const createGrid = (size) => Array(size).fill(0).map(() => ["#fff", "#fff", "#fff", "#fff"]);
-const Game = ({color}) => {
-    const [grid, setGrid] = useState(createGrid(10));
-    const [currentRow, setCurrentRow] = useState(0);
+const createGrid = (size) =>
+  Array(size)
+    .fill(0)
+    .map(() => ["#fff", "#fff", "#fff", "#fff"]);
 
-    return (
-        <div className="space-y-2">
-            {grid.map((row, i) => (
-              <div key={i} className={`border ${currentRow == i ? 'border-black' : 'border-red-500'} h-14 w-full p-2 relative`}>
-                <Row colors={row} />
-              </div>
-            ))}
+const Game = ({ pickedColor }) => {
+  const [grid, setGrid] = useState(createGrid(10));
+  const [currentRow, setCurrentRow] = useState(0);
+
+  const changeColor = ({ row, col }) => {
+    if (currentRow == row) {
+      const temp = [...grid];
+      temp[row][col] = pickedColor;
+      setGrid(temp);
+    }
+  };
+
+  return (
+    <div className="space-y-2">
+      {grid.map((row, rowNo) => (
+        <div
+          key={rowNo}
+          className={`border ${
+            currentRow == rowNo ? "border-black" : "border-red-500"
+          } h-14 w-full p-2 relative`}
+        >
+          <Row
+            colors={row}
+            row={rowNo}
+            picked={pickedColor}
+            getCell={changeColor}
+          />
         </div>
-    )
+      ))}
+    </div>
+  );
 };
 
 export default Game;
--- components/Row.js
+++ components/Row.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const SingleRow = ({ colors }) => {
+const Row = ({ colors,  getCell, row }) => {
   return (
     <>
       <div className="absolute inset-0 left-3 top-2">
         <div className="flex space-x-4">
-          {colors.map((color) => (
-            <>
+          {colors.map((color, col) => (
               <div
+                key={col}
                 className="border border-black rounded-full h-10 w-10"
                 style={{ background: color }}
+                onClick={() => getCell({row, col})}
               ></div>
-            </>
           ))}
         </div>
       </div>
@@ -19,4 +19,4 @@ const SingleRow = ({ colors }) => {
   );
 };
 
-export default SingleRow;
+export default Row;
