3ffa1a5889e61779b6d945250380336cbddc98d6
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-15 13:56:38 +0530

    added header and wrapped the colors

--- components/ColorPalette.js
+++ components/ColorPalette.js
@@ -9,18 +9,21 @@ const ColorPalette = ({ selectedColor }) => {
   };
 
   return (
-    <div>
-      {colors.map((color, colNo) => (
-        <div
-          key={colNo}
-          className={`h-10 w-10 rounded-full border ${
-            picked == color ? "border-black" : ""
-          }`}
-          style={{ background: `${color}` }}
-          onClick={() => setPickedColor(color)}
-        ></div>
-      ))}
-    </div>
+    <>
+      <div className="text-xl px-4 pt-4">Pick any color:</div>
+      <div className="flex flex-wrap justify-center p-4">
+        {colors.map((color, colNo) => (
+          <div
+            key={colNo}
+            className={`h-10 w-10 rounded-full border-2 m-2 ${
+              picked == color ? "border-black" : ""
+            }`}
+            style={{ background: `${color}` }}
+            onClick={() => setPickedColor(color)}
+          ></div>
+        ))}
+      </div>
+    </>
   );
 };
 
