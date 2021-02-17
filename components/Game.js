1051d59aa979ded7b6761390eb7d02474a5bc140
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-17 23:10:42 +0530

    added check mark after filling row and validate filling the 4 small pegs with black, red, white color

--- components/Game.js
+++ components/Game.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Row from "../components/Row";
-import { cloneDeep } from "lodash";
+import { isEqual, cloneDeep } from "lodash";
 
 const createGrid = (size) =>
   Array(size)
@@ -30,8 +30,32 @@ const Game = ({ pickedColor }) => {
   };
 
   const checkRow = () => {
+    updateRowStatus();
     setCurrentRow(currentRow + 1);
     setRowStatus(false);
+  };
+
+  const updateRowStatus = () => {
+    const newGrid = cloneDeep(grid);
+    const newStatusGrid = cloneDeep(gridStatus);
+    const curRow = newGrid[currentRow];
+    if (isEqual(gameState, curRow)) {
+      alert("you won");
+      return;
+    }
+    const currentRowStatus = ["#fff", "#fff", "#fff", "#fff"];
+    curRow.forEach((cur, i) => {
+      if (gameState[i] == cur) currentRowStatus[i] = "red";
+    });
+    curRow.forEach((cur, i) => {
+      if (currentRowStatus[i] == "#fff") {
+        const index = gameState.indexOf(cur);
+        if (index != -1 && i != index && currentRowStatus[index] == "#fff")
+          currentRowStatus[index] = "black";
+      }
+    });
+    newStatusGrid[currentRow] = currentRowStatus;
+    setGridStatus(newStatusGrid);
   };
 
   return (
@@ -52,7 +76,7 @@ const Game = ({ pickedColor }) => {
           />
           {currentRow == rowNo && rowStatus && (
             <div className="absolute left-64 inset-y-1/4">
-				<img onClick={checkRow} src="check.svg" className="h-6" />
+              <img onClick={checkRow} src="check.svg" className="h-6" />
             </div>
           )}
         </div>
