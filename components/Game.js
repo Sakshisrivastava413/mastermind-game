6c41e1bb407434e694e4bf13516784d80f8c34aa
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-18 14:15:09 +0530

    MAJOR: checking row status, reseting if done else generating indexes and storing hints at random index

--- components/Game.js
+++ components/Game.js
@@ -1,61 +1,107 @@
 import React, { useState } from "react";
 import Row from "../components/Row";
-import { isEqual, cloneDeep } from "lodash";
+import { isEqual, cloneDeep, random } from "lodash";
 
-const createGrid = (size) =>
-  Array(size)
+const createGrid = (rows, cols, value) =>
+  Array(rows)
     .fill(0)
-    .map(() => ["#fff", "#fff", "#fff", "#fff"]);
+    .map(() => new Array(cols).fill(0).map(() => value));
 
 const gameState = [
-  "#F545F3", //pink
-  "#45F5F5", //lignt-blue
-  "#895252", //brown
-  "#B4ADAD", // grey
+  "#FC7DEF", //pink
+  "#F0B27A", //orange
+  "#CA6F1E", //brown
+  "#CACFD2", // grey
 ];
 
-const Game = ({ pickedColor }) => {
-  const [grid, setGrid] = useState(createGrid(10));
+const Game = ({ pickedColor, result }) => {
+  const [grid, setGrid] = useState(createGrid(10, 4, "#fff"));
   const [currentRow, setCurrentRow] = useState(0);
-  const [rowStatus, setRowStatus] = useState(false);
-  const [gridStatus, setGridStatus] = useState(createGrid(10));
+  const [rowState, setRowDone] = useState(false);
+  const [gridStatus, setGridStatus] = useState(createGrid(10, 4, "#fff"));
 
   const changeColor = ({ row, col }) => {
     if (currentRow == row) {
       const newGrid = cloneDeep(grid);
       newGrid[row][col] = pickedColor;
       setGrid(newGrid);
-      if (newGrid[currentRow].indexOf("#fff") == -1) setRowStatus(true);
+      if (newGrid[currentRow].indexOf("#fff") == -1) setRowDone(true);
     }
   };
 
   const checkRow = () => {
-    updateRowStatus();
-    setCurrentRow(currentRow + 1);
-    setRowStatus(false);
+    const [colorPosMatch, colorMatch] = getRowStatus();
+    updateRowStatus(colorPosMatch, colorMatch);
+    setRowDone(false);
   };
 
-  const updateRowStatus = () => {
+  const getRowStatus = () => {
     const newGrid = cloneDeep(grid);
-    const newStatusGrid = cloneDeep(gridStatus);
     const curRow = newGrid[currentRow];
-    if (isEqual(gameState, curRow)) {
-      alert("you won");
-      return;
+    const nextRow = currentRow + 1;
+    let colorAndPositionMatchCount = 0;
+    let onlyColorMatchCount = 0;
+    const colorFreq = new Map();
+
+    if (isEqual(gameState, curRow) || nextRow == 10) {
+      result();
+      resetGame();
+    } else {
+      setCurrentRow(nextRow);
+
+      curRow.forEach((color, col) => {
+        const gameStateColor = gameState[col];
+        if (gameStateColor === color) colorAndPositionMatchCount++;
+        else
+          colorFreq.set(
+            gameStateColor,
+            colorFreq.has(gameStateColor) ? colorFreq.get(gameStateColor) + 1 : 1
+          );
+      });
+
+      curRow.forEach((color, col) => {
+        if (
+          colorFreq.has(color) &&
+          colorFreq.get(color) > 0 &&
+          color != gameState[col]
+        ) {
+          colorFreq.set(color, colorFreq.get(color) - 1);
+          onlyColorMatchCount++;
+        }
+      });
     }
-    const currentRowStatus = ["#fff", "#fff", "#fff", "#fff"];
-    curRow.forEach((cur, i) => {
-      if (gameState[i] == cur) currentRowStatus[i] = "red";
-    });
-    curRow.forEach((cur, i) => {
-      if (currentRowStatus[i] == "#fff") {
-        const index = gameState.indexOf(cur);
-        if (index != -1 && i != index && currentRowStatus[index] == "#fff")
-          currentRowStatus[index] = "black";
+    return [colorAndPositionMatchCount, onlyColorMatchCount];
+  };
+
+  const updateRowStatus = (colorPosMatch, colorMatch) => {
+    if(!colorPosMatch && !colorMatch) return;
+    const newStatusGrid = cloneDeep(gridStatus);
+    const rowStatus = createGrid(1, 4, '#fff')[0];
+
+    while(colorPosMatch) {
+      const index = random(0, 4);
+      if(rowStatus[index] === '#fff') {
+        rowStatus[index] = 'red';
+        colorPosMatch--;
       }
-    });
-    newStatusGrid[currentRow] = currentRowStatus;
+    }
+
+    while(colorMatch) {
+      const index = random(0, 4);
+      if(rowStatus[index] === '#fff') {
+        rowStatus[index] = 'black';
+        colorMatch--;
+      }
+    }
+
+    newStatusGrid[currentRow] = rowStatus;
     setGridStatus(newStatusGrid);
+  };
+
+  const resetGame = () => {
+    setGrid(createGrid(10, 4, "#fff"));
+    setGridStatus(createGrid(10, 4, "#fff"));
+    setCurrentRow(0);
   };
 
   return (
@@ -63,9 +109,9 @@ const Game = ({ pickedColor }) => {
       {grid.map((row, rowNo) => (
         <div
           key={rowNo}
-          className={`${
-            currentRow == rowNo ? "border-2" : "border"
-          } border-black h-14 w-full p-2 relative`}
+          className={`${currentRow == rowNo ? "border-2" : "border"} ${
+            currentRow != rowNo ? "border-gray-100" : "border-black"
+          } h-14 w-full p-2 relative`}
         >
           <Row
             colors={row}
@@ -73,10 +119,15 @@ const Game = ({ pickedColor }) => {
             picked={pickedColor}
             getCell={changeColor}
             status={gridStatus[rowNo]}
+            activeRow={currentRow}
           />
-          {currentRow == rowNo && rowStatus && (
+          {currentRow == rowNo && rowState && (
             <div className="absolute left-64 inset-y-1/4">
-              <img onClick={checkRow} src="check.svg" className="h-6" />
+              <img
+                onClick={checkRow}
+                src="check.svg"
+                className="h-6 cursor-pointer"
+              />
             </div>
           )}
         </div>
