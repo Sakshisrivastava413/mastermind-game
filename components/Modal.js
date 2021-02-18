2f5e0c9c541f6c10f12f608d616315f5352ae82b
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-18 17:40:21 +0530

    removed react modal warning and used proper name convention

--- components/Modal.js
+++ components/Modal.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactModal from "react-modal";
 
-const Modal = ({ status, info, handleClose }) => {
-  return (
-    <ReactModal
-      isOpen={status}
-      onRequestClose={handleClose}
-      className="absolute m-auto inset-0  h-96 w-96 border border-black outline-none"
-    >
-      <div className="bg-white h-full">{info()}</div>
-    </ReactModal>
-  );
-};
+const Modal = ({ isOpen, children, onClose }) => (
+  <ReactModal
+    isOpen={isOpen}
+    onRequestClose={onClose}
+    ariaHideApp={false}
+    className="absolute m-auto inset-0  h-96 w-96 border border-black outline-none"
+  >
+    <div className="bg-white h-full">{children}</div>
+  </ReactModal>
+);
 
 export default Modal;
