c21daca973f6f7f8b9e0081362ee98543b676b99
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-18 14:13:01 +0530

    removed modal timeout on close

--- components/Modal.js
+++ components/Modal.js
@@ -4,7 +4,6 @@ import ReactModal from "react-modal";
 const Modal = ({ status, info, handleClose }) => {
   return (
     <ReactModal
-      closeTimeoutMS={500}
       isOpen={status}
       onRequestClose={handleClose}
       className="absolute m-auto inset-0  h-96 w-96 border border-black outline-none"
