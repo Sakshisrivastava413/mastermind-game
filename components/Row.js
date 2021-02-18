3056419de2c13c100815735a3303a9f7bca5ad78
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-18 14:12:40 +0530

    changed grid status structure. Just storing color values

--- components/Row.js
+++ components/Row.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Row = ({ colors, getCell, row, status, activeRow = false }) => {
+const Row = ({ colors, getCell, row, status, activeRow }) => {
   return (
     <>
       <div className="absolute inset-0 left-3 inset-y-2.5">
@@ -8,7 +8,7 @@ const Row = ({ colors, getCell, row, status, activeRow = false }) => {
           {colors.map((color, col) => (
             <div
               key={col}
-              className={`border border-black rounded-full h-9 w-9 ${activeRow ? 'cursor-pointer' : ''}`}
+              className={`border border-black rounded-full h-9 w-9 ${activeRow == row ? 'cursor-pointer' : ''}`}
               style={{ background: color }}
               onClick={() => getCell({ row, col })}
             ></div>
@@ -17,14 +17,14 @@ const Row = ({ colors, getCell, row, status, activeRow = false }) => {
       </div>
       <div className="absolute right-3">
         <div className="w-10 flex flex-wrap">
-          {status.map((cell, col) => (
+          {status.map((color, col) => (
             <div
               key={col}
               className="m-1 border border-black rounded-full h-3 w-3"
-              style={{ background: cell.color }}
+              style={{ background: color }}
             >
               {
-                cell.color == '#fff' && cell.validate && <img className="m-auto h-2" src="cancel.svg" />
+                color == '#fff' && row < activeRow && <img className="m-auto h-2" src="cancel.svg" />
               }
             </div>
           ))}
