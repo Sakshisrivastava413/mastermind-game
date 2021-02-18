7fc0957b7b2b8423b7726a4b907cb6f884b84fc4
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-18 23:26:09 +0530

    made the container width and height responsive

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => (
-  <nav className="bg-blue-500 py-5 px-12">
+  <nav className="bg-blue-500 py-4 px-12 h-16">
     <div className="text-center text-white text-3xl">mastermind</div>
   </nav>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "112": "28rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
