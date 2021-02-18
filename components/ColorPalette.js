59cd5ffdd32d37ae831b062cdf889e492ca8033a
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2021-02-18 17:41:58 +0530

    used parent state instead of creating again in child component

--- components/ColorPalette.js
+++ components/ColorPalette.js
@@ -1,30 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import colors from "../colors.constant";
 
-const ColorPalette = ({ selectedColor }) => {
-  const [picked, setPicked] = useState("");
-  const setPickedColor = (color) => {
-    setPicked(color);
-    selectedColor(color);
-  };
-
-  return (
-    <>
-      <div className="text-xl px-4 pt-4 text-center font-mono">Color choices</div>
-      <div className="flex flex-wrap justify-center p-4">
-        {colors.map((color, colNo) => (
-          <div
-            key={colNo}
-            className={`h-10 w-10 rounded-full border-2 m-2 cursor-pointer ${
-              picked == color ? "border-black" : ""
-            }`}
-            style={{ background: `${color}` }}
-            onClick={() => setPickedColor(color)}
-          ></div>
-        ))}
-      </div>
-    </>
-  );
-};
+const ColorPalette = ({ selectedColor, onColorSelected }) => (
+  <>
+    <div className="text-xl px-4 pt-4 text-center">Color choices</div>
+    <div className="flex flex-wrap justify-center p-4">
+      {colors.map((color, colNo) => (
+        <div
+          key={colNo}
+          className={`h-10 w-10 rounded-full border-2 m-2 cursor-pointer ${
+            selectedColor === color ? "border-black" : ""
+          }`}
+          style={{ background: `${color}` }}
+          onClick={() => onColorSelected(color)}
+        ></div>
+      ))}
+    </div>
+  </>
+);
 
 export default ColorPalette;
